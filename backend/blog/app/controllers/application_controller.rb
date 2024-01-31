0dfe284bc83d60e91c69082cb08f9739fdd596d4
ashtonyoung <youngashton28@gmail.com>
2024-01-30 21:17:01 -0700

    trying out standard??

--- backend/blog/app/controllers/application_controller.rb
+++ backend/blog/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 # Application Controller for my blog app @rubocop
 class ApplicationController < ActionController::API
+  private
+
+  # Finds the User with the ID stored in the session with the key
+  # :current_user_id This is a common way to handle user login in
+  # a Rails application; logging in sets the session value and
+  # logging out removes it.
+  def current_user
+    @_current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
+  end
 end
