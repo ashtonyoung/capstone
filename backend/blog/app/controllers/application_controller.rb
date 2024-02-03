ef4cb5ded582e5743a785d35916089f0d2bd8ae5
ashtonyoung <youngashton28@gmail.com>
2024-02-03 14:16:02 -0700

    sessions working with frontend at LogInView.vue

--- backend/blog/app/controllers/application_controller.rb
+++ backend/blog/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 # Application Controller for my blog app @rubocop
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:create]
+
   private
 
   # Finds the User with the ID stored in the session with the key
@@ -8,5 +11,18 @@ class ApplicationController < ActionController::API
   # logging out removes it.
   def current_user
     @_current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    current_user.present?
+  end
+
+  # Method to use in a before_action to require that a user is
+  # logged in to access a given route.
+  def authenticate_user!
+    unless logged_in?
+      render json: {error: "You must be logged in to access this section."}, status: :unauthorized
+    end
   end
 end
