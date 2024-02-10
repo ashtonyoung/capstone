05f622b2e81015522f699317e08db8d29f46020d
ashtonyoung <youngashton28@gmail.com>
2024-02-10 13:40:47 -0700

    setup two font families

--- frontend/vue-app/tailwind.config.js
+++ frontend/vue-app/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // use inter for font family
+        sans: ['Inter', 'sans-serif'],
+        serif: ['DM Serif Display', 'serif'],
+      },
+      colors: {
+        primary: '#0000FF',
+      },
+    },
   },
   plugins: [],
 }
