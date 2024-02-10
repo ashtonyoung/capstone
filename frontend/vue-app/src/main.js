5914a96db4061873fc3db60daf171dc52e9c789a
ashtonyoung <youngashton28@gmail.com>
2024-02-10 16:44:06 -0700

    register Ev components

--- frontend/vue-app/src/main.js
+++ frontend/vue-app/src/main.js
@@ -4,7 +4,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
 
+// Ev Components
+import EvModal from '@/components/generic/EvModal.vue'
+import EvInputField from '@/components/generic/EvInputField.vue'
+import EvBtn from '@/components/generic/EvBtn.vue'
+
 const app = createApp(App)
+
+//register components
+app.component('ev-modal', EvModal)
+app.component('ev-input-field', EvInputField)
+app.component('ev-btn', EvBtn)
 
 app.use(router)
 
