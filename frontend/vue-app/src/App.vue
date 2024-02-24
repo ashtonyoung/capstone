444364e0de80edd152349034f40cee4109bb8373
ashtonyoung <youngashton28@gmail.com>
2024-02-24 13:35:53 -0700

    Added dynamic NavBar

--- frontend/vue-app/src/App.vue
+++ frontend/vue-app/src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
   <header></header>
 
-  <RouterView />
+  <NavBar v-if="$route.path !== '/login'" />
+  <!--  Don't want global margins on Nav  -->
+  <section
+    id="router-view-container"
+    class="mx-2 sm:mx-4"
+  >
+    <RouterView />
+  </section>
 </template>
 
 <style>
