adc9b66deeb93cf0de46b8a75ba1a86704f5721c
ashtonyoung <youngashton28@gmail.com>
2024-03-10 13:18:57 -0600

    events_controller.rb added index + create methods

--- backend/blog/app/controllers/events_controller.rb
+++ backend/blog/app/controllers/events_controller.rb
@@ -1,15 +1,41 @@
 class EventsController < ApplicationController
-  before_action :set_context
   def index
-    @events = @user.events
+    @user = User.find_by(handle: params[:handle])
+    if @user.blank?
+      render json: {error: "User not found"}, status: :not_found
+      return
+    end
+    @metadata = {for_current_user: for_current_user(user_id: @user.id)}
+
+    @events = @user.events.order(start_date: :desc)
     @events.map { |e| e.as_json }
     render json: {metadata: @metadata, events: @events}
   end
 
+  def create
+    @event = Event.new(user_id: current_user.id, **event_params)
+
+    if @event.save
+      render json: @event, status: :created
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    render json: @event.as_json(include_nil_values: true)
+  end
+
   private
 
-  def set_context
-    @user = User.find_by(handle: params[:handle])
-    @metadata = {for_current_user: for_current_user(user_id: @user.id)}
+  def event_params
+    params.require(:event).permit(
+      %i[
+        name
+        description
+        start_date
+      ]
+    )
   end
 end
